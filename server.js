7fc7c01b1a23f6b562e3e1a244d615aa87abb5b1
ayarde <adrian.ayarde@gmail.com>
2017-03-25 19:12:12 -0300

    Use regular expression like file path parameter, in http get.

--- server.js
+++ server.js
@@ -4,28 +4,12 @@ var app = express();
 
 var apiurl = express.Router();
 
-var http = require("http");
-var url = require("url");
-
-/*apiurl.use(function(req,res,next){
-var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-console.log('url',fullUrl);
-next();
-console.log('Url',fullUrl);
-
-});
-app.use('/',apiurl);*/
-
-
 var allowMethods = function(req, res, next){
   res.header("Access-Control-Allows-Methods", "GET, POST, PUT, DELETE");
 }
 
-app.get('/cycleDetection',function(req, res, next){
-  //var pathname = url.parse(req.url).pathname;
-  //console.log("Request for " + pathname + " received.");
-  res.send('Hello World');
-
+app.get('/cycleDetection/:path(*\*)',function(req, res, next){
+  res.send(req.param('path'));
 });
 
 app.listen(8080, function(){
